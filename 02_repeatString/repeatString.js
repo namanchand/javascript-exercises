2ed400867d3b77464ac5a0161ed5429ce3abefc7
namanchand <naman.omd@gmail.com>
2024-08-27 23:58:21 +0530

    completed 7 test in the repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let result = "";
+  if (number < 0) {
+    result = `ERROR`;
+  } else {
+    for (let i = 0; i < number; i++) {
+      result = string + result;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
