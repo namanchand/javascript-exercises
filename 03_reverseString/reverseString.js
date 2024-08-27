96574704296374c51a073aa9fe2fa508646db466
namanchand <naman.omd@gmail.com>
2024-08-28 01:01:13 +0530

    Challenge 3 complete reverseString function test complete on all 4 tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let reverseString = "";
+  for (let i = 0; i < word.length; i++) {
+    reverseString += word[word.length - i - 1];
+  }
+  console.log(reverseString);
+  return reverseString;
 };
 
 // Do not edit below this line
