d4325433ab9c08a6a310b99d05a2549fa80b1c20
namanchand <naman.omd@gmail.com>
2024-08-29 00:29:56 +0530

    Exercise 6 complete
    all test passed.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let result = "";
+  if (year % 400 === 0 && year % 100 === 0) {
+    result = true;
+  } else if (year % 100 === 0) {
+    result = false;
+  } else if (year % 4 === 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
