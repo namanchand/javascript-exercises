8df0042628fd759a8fb895a0e7866ce9012428fe
namanchand <naman.omd@gmail.com>
2024-08-28 20:05:32 +0530

    completed challenge no. 4
    finished all 8 test of the exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr1, ...arr2) {
+  let newArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    let addIt = true;
+
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        addIt = false;
+        break;
+      }
+    }
+    if (addIt) newArr.push(arr1[i]);
+  }
+  console.log(newArr);
+  return newArr;
 };
 
 // Do not edit below this line
