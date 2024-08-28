b7e0a6971e1ac7cb466dab9eca8d895479ba3527
namanchand <naman.omd@gmail.com>
2024-08-28 22:53:55 +0530

    Completed Exercise number 5
    all test passed in the exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let output = 0;
+  if (
+    Number.isInteger(num1) &&
+    num1 > 0 &&
+    Number.isInteger(num2) &&
+    num2 > 0
+  ) {
+    if (num1 < num2) {
+      for (let i = num1; i <= num2; i++) {
+        output += i;
+      }
+    } else if (num1 > num2) {
+      for (let i = num2; i <= num1; i++) {
+        output += i;
+      }
+    }
+  } else {
+    output = "ERROR";
+  }
+  console.log(output);
+  return output;
 };
 
 // Do not edit below this line
