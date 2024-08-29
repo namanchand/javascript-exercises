2f44076b28af72df53549bec719e75259aa23a85
namanchand <naman.omd@gmail.com>
2024-08-29 10:46:17 +0530

    Exercise 7 complete All test passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  let tempCelsius = Math.round((F - 32) * (5 / 9) * 10) / 10;
+  return Number(tempCelsius);
 };
-
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  let tempFahrenheit = Math.round(((9 / 5) * C + 32) * 10) / 10;
+  return Number(tempFahrenheit);
 };
-
-// Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
