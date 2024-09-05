6483520542a6fd5a720f82d684856bca82cbbdd3
namanchand <naman.omd@gmail.com>
2024-09-06 00:57:24 +0530

    completed exercise no. 8,9,10
    Calculator, Palindromes and Fibonacci test exercises complete.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    let num = parseInt(index)
+    let array = [0, 1];
+    let result;
 
+    if (num === 0) {
+        return result = array[0]
+    } else if (num < 0) {
+        return result = "OOPS"
+    } else if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            result = (array[array.length - 1] + array[array.length - 2])
+            array.push(result)
+        } return array[num]
+    }
+    console.log(result);
 };
 
 // Do not edit below this line
